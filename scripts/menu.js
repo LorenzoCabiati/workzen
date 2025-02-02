86deccd73297f245eb04f8dfff68532a9f1046fe
LorenzoCabiati <lollo.cabiatida04@gmail.com>
2025-02-02 14:54:24 +0100

    fix vari

--- scripts/menu.js
+++ scripts/menu.js
@@ -8,11 +8,26 @@ let btn5 = document.getElementById("m-bottom-2");
 
 btn1.addEventListener("click", function(){
     iFrame.src="pages/home.html"
+
+    removeClassFromAll("m-option-selected");
+    btn1.classList.add("m-option-selected")
 })
 
 btn4.addEventListener("click", function(){
     iFrame.src="pages/settings.html"
+
+    removeClassFromAll("m-option-selected");
+    btn4.classList.add("m-option-selected")
 })
 btn5.addEventListener("click", function(){
     iFrame.src="pages/login.html"
+
+    removeClassFromAll("m-option-selected");
+    btn5.classList.add("m-option-selected")
 })
+
+function removeClassFromAll(className) {
+    document.querySelectorAll(`.${className}`).forEach(element => {
+        element.classList.remove(className);
+    });
+}
